f6585a11255db0377a18a03d0610066d0a6283b2
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-08-07 21:56:23 +0300

    FINISH THE TASK

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,43 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  let condition, first, second;
 
+  beforeEach(() => {
+    condition = jest.fn();
+    first = jest.fn();
+    second = jest.fn();
   });
 
-  // write tests here
+  it('does not return anything from the function', () => {
+    const result = ifElse(condition, first, second);
+
+    expect(result).toBeUndefined();
+  });
+
+  it('evaluates the condition exactly once', () => {
+    ifElse(condition, first, second);
+
+    expect(condition).toHaveBeenCalledTimes(1);
+  });
+
+  it('executes the "first" callback when condition returns true', () => {
+    const trueCondition = jest.fn().mockReturnValue(true);
+
+    ifElse(trueCondition, first, second);
+
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('executes the "second" callback when condition returns false', () => {
+    const falseCondition = jest.fn().mockReturnValue(false);
+
+    ifElse(falseCondition, first, second);
+
+    expect(second).toHaveBeenCalledTimes(1);
+    expect(first).not.toHaveBeenCalled();
+  });
 });
